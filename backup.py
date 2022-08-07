14d88ca230019cc76de8dc1e920e8094f8488a59
Keven <kevenluan.sb@hotmail.com>
2022-08-07 20:39:46 -0300

    feito o segundo teste

--- backup.py
+++ backup.py
@@ -1,7 +1,6 @@
 import os
 import time
 import shutil
-import sys
 
 
 # ver ultima modificacao de um arquivo
@@ -9,4 +8,10 @@ def last_modified(path):
     return time.ctime(os.path.getmtime(path))
 
 
+# verificar quais dos arquivos tem a ultima modificacao
+def which_is_newest(path1, path2):
+    if last_modified(path1) > last_modified(path2):
+        return path1
+    else:
+        return path2
 
--- testa_backup.py
+++ testa_backup.py
@@ -4,3 +4,5 @@ import backup
 def test_last_modified():
     assert backup.last_modified('MP_trab_3.pdf') == 'Sun Aug  7 17:59:19 2022'
 
+def test_which_is_newest():
+    assert backup.which_is_newest('MP_trab_3.pdf', 'Aula_Testes_2.pdf') == 'Aula_Testes_2.pdf'
