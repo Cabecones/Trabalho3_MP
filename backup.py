c480e552747e0ab9df92b787d544b5ed6b0fe590
Keven <kevenluan.sb@hotmail.com>
2022-08-14 03:35:57 -0300

    Casos de teste 2 e 3 prontos

--- backup.py
+++ backup.py
@@ -3,7 +3,7 @@ import time
 import shutil
 
 
-# verificar se backupParm.txt está vazio
+# verificar se path_backup_parm está vazio
 def is_empty(path):
     result = os.stat(path).st_size == 0
     if result != 0:
@@ -30,8 +30,70 @@ def has_file(path, file):
     return os.path.isfile(os.path.join(path, file))
 
 
+# extrair texto do path_backup_parm
+def get_text(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
 # copia um arquivo para outro diretorio
 def copy_file(path1, path2):
     shutil.copy(path1, path2)
+
+
+def program(path_hd, path_pendrive, path_backup_parm, faz_backup):
+    # primeiro verifica se parm existe
+    if is_empty(path_backup_parm) != 'Impossível':
+        # verifica se o arquivo existe em hd
+        if has_file(path_hd, get_text(path_backup_parm)):
+            if has_file(path_pendrive, get_text(path_backup_parm)):
+                if faz_backup == 'True':
+                    # verifica qual eh o arquivo mais recente
+                    if which_is_newest(path_hd + '/' + get_text(path_backup_parm),
+                                       path_pendrive + '/' + get_text(path_backup_parm)) == path_hd + '/' + get_text(
+                        path_backup_parm):
+                        # copia o arquivo para pendrive
+                        copy_file(path_hd + '/' + get_text(path_backup_parm), path_pendrive)
+                        print('Arquivo copiado para o pendrive')
+                        # verifica se os dois tem o mesmo tempo de modificacao
+                        if last_modified(path_hd + '/' + get_text(path_backup_parm)) == last_modified(
+                                path_pendrive + '/' + get_text(
+                                    path_backup_parm)):
+                            return 'Faz nada'
+                    else:
+                        print('Não foi possível fazer o backup')
+                else:
+                    # verifica qual eh o arquivo mais recente
+                    if which_is_newest(path_hd + '/' + get_text(path_backup_parm),
+                                       path_pendrive + '/' + get_text(path_backup_parm)) == path_hd + '/' + get_text(
+                        path_backup_parm):
+                        print('Não foi possível fazer o backup')
+                        # verifica se os dois tem o mesmo tempo de modificacao
+                        if last_modified(path_hd + '/' + get_text(path_backup_parm)) == last_modified(
+                                path_pendrive + '/' + get_text(
+                                    path_backup_parm)):
+                            return 'Faz nada'
+                    else:
+                        copy_file(path_pendrive + '/' + get_text(path_backup_parm), path_hd)
+            else:
+                if faz_backup == 'True':
+                    # copia o arquivo para o pendrive
+                    copy_file(path_hd + '/' + get_text(path_backup_parm), path_pendrive)
+                    print('Arquivo copiado para o pendrive')
+                else:
+                    print('Não foi possível fazer o backup')
+        else:
+            if faz_backup == 'True':
+                print('Não foi possível fazer o backup')
+                if has_file(path_pendrive, get_text(path_backup_parm)):
+                    return 'Faz nada'
+            else:
+                if has_file(path_pendrive, get_text(path_backup_parm)):
+                    copy_file(path_pendrive + '/' + get_text(path_backup_parm), path_hd)
+                    print('Arquivo copiado para o hd')
+                else:
+                    print('Não foi possível fazer o backup')
+    else:
+        print('Impossível')
 
 
--- testa_backup.py
+++ testa_backup.py
@@ -1,10 +1,29 @@
+import sys
+
 import backup
 
-#teste caso 1 - check backupParm
-def testa_check_backupParm():
+faz_backup_s = True
+faz_backup_n = False
+
+
+# teste caso 1 - check backupParm
+def test_check_backupParm():
     assert backup.is_empty('casos_teste/caso1/backupParm.txt') == ''
     assert backup.is_empty('casos_teste/caso1/backupParm_vazio.txt') == 'Impossível'
 
 
-#teste caso 2
+# teste caso 2
+def test_caso2():
+    # testar se o arquivo de pendrive foi copiado para pendrive
+    backup.program('casos_teste/caso2/hd', 'casos_teste/caso2/pendrive', 'casos_teste/caso2/backupParm.txt', 'True')
+    assert backup.has_file('casos_teste/caso2/pendrive', backup.get_text('casos_teste/caso2/backupParm.txt')) == True
+
+
+# teste caso 3
+def test_caso3(capsys):
+    # testar no caso do arquivo do hd ser mais recente
+    backup.program('casos_teste/caso3/hd', 'casos_teste/caso3/pendrive', 'casos_teste/caso3/backupParm.txt', 'True')
+    captured = capsys.readouterr()
+    assert captured.out == "Arquivo copiado para o pendrive\n"
+
 
